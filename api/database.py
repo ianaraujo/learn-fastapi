382a27c725e14019678b9b72537bfcd4e77148da
Ian Araujo <ianaraujo15@gmail.com>
2024-05-13 12:00:19 -0300

    Create models and database

--- api/database.py
+++ api/database.py
@@ -7,4 +7,5 @@ DATABASE_URL = "sqlite:///./learn.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
--- api/models.py
+++ api/models.py
@@ -1,19 +1,48 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Table, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 from database import Base
 
-class Minuta(Base):
-    __tablename__ = "minutas"
+
+# association tables
+
+stakeholder_minuta = Table(
+    'stakeholder_minuta', Base.metadata,
+    Column('stakeholder_id', Integer, ForeignKey('stakeholders.id'), primary_key=True),
+    Column('minuta_id', Integer, ForeignKey('minutas.id'), primary_key=True)
+)
+
+issue_minuta = Table(
+    'issue_minuta', Base.metadata,
+    Column('issue_id', Integer, ForeignKey('issues.id'), primary_key=True),
+    Column('minuta_id', Integer, ForeignKey('minutas.id'), primary_key=True)
+)
+
+# models
+
+class Stakeholder(Base):
+    __tablename__ = "stakeholders"
 
     id = Column(Integer, primary_key=True)
-    header = Column(String, nullable=False)
-    false = Column(String, nullable=False)
+    name = Column(String, nullable=False)
 
-    issues = relationship("Issue", back_populates="id")
 
 class Issue(Base):
     __tablename__ = "issues"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, index=True)
+    name = Column(String, nullable=False)
+
+
+class Minuta(Base):
+    __tablename__ = "minutas"
+
+    id = Column(Integer, primary_key=True)
+    author = Column(Date, nullable=False)
+    created_at = Column(Date, default=func.now())
+    updated_at = Column(Date, onupdate=func.now())
+    header = Column(String, nullable=False)
+    body = Column(String, nullable=False)
+    stakeholders = relationship("Stakeholder", secondary=stakeholder_minuta, back_populates="minutas")
+    issues = relationship("Issue", secondary=issue_minuta, back_populates="minutas")
